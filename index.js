f24182cfe62dafc3e9e9da715e8c38c767297069
hyytiala <otto.hyytiala@gmail.com>
2018-09-06 13:55:30 +0300

    Simple app that returns the whole program

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const request = require('request');
+
+  app.get('/', (req, res) => {
+    request.get({
+        uri: 'https://pof-backend.partio.fi/spn-ohjelma-json-taysi',
+        strictSSL: false
+    }).pipe(res);
+  })
+  
+  const PORT = 3001
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
