95766952780fab703da6e4858818569f4f2cbe2e
hyytiala <otto.hyytiala@gmail.com>
2018-09-10 16:05:19 +0300

    playing with the API JSON

--- index.js
+++ index.js
@@ -2,11 +2,27 @@ const express = require('express')
 const app = express()
 const request = require('request')
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
   request.get({
     uri: 'https://pof-backend.partio.fi/spn-ohjelma-json-taysi',
     strictSSL: false
   }).pipe(res)
+})
+
+app.get('/api/tarpojat', (req, res) => {
+  request.get({
+    uri: 'https://pof-backend.partio.fi/spn-ohjelma-json-taysi',
+    strictSSL: false
+  }, function (error, response, body) {
+    if (error == null) {
+      const json = JSON.parse(body);
+      res.send(json.program[0].agegroups[2]);
+    }
+  })
+})
+
+app.get('/hello', (req, res) => {
+  res.end('Hello world')
 })
 
 const PORT = 3001
