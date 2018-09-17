7d920613c9392168b2aad1750c63e2fa8c9a9ff0
hyytiala <otto.hyytiala@gmail.com>
2018-09-17 15:25:49 +0300

    Adding suggestions and task details to returned JSON

--- controllers/pof.js
+++ controllers/pof.js
@@ -1,20 +1,65 @@
 const pofRouter = require('express').Router()
 const request = require('request')
+const axios = require('axios')
 
 pofRouter.get('/', async (req, res) => {
   res.send('Hello world')
 })
 
 pofRouter.get('/full', async (req, res) => {
-  request.get({
-    uri: 'https://pof-backend.partio.fi/spn-ohjelma-json-taysi',
-    strictSSL: false
-  }, function (error, response, body) {
-    if (error == null) {
-      const json = JSON.parse(body)
+  const url = 'https://pof-backend.partio.fi/spn-ohjelma-json-taysi'
+  const data = await getContent(url)
+  const tarpojat = data.program[0].agegroups[2]
+  async function taskDetails() {
+    for (const taskgroup of tarpojat.taskgroups) {
+      for (const task of taskgroup.tasks) {
+        const details = await getTaskDetails(task)
+        Object.assign(task, details)
+      }
     }
-  })
+    suggestions()
+  }
+  async function suggestions() {
+    for (const taskgroup of tarpojat.taskgroups) {
+      for (const task of taskgroup.tasks) {
+        task.suggestions_details = await getSuggestions(task)
+      }
+    }
+    res.json(tarpojat)
+  }
+  taskDetails()
 })
+
+const getSuggestions = async task => {
+  try {
+    const response = await axios.get(task.suggestions_details[0].details)
+    const data = response.data.items
+    return data
+  } catch (error) {
+    return []
+  }
+}
+
+const getTaskDetails = async task => {
+  try {
+    const response = await axios.get(task.languages[0].details)
+    const data = response.data
+    return data
+  } catch (error) {
+    return null
+  }
+}
+
+const getContent = async url => {
+  try {
+    const response = await axios.get(url)
+    const data = response.data
+    return data
+  } catch (error) {
+    return ''
+  }
+}
+
 
 pofRouter.get('/tarppo', (req, res) => {
   request.get({
