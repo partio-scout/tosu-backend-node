fa826fba3bf631061ef09234a29bc5131643f499
virtalas <virtalas@users.noreply.github.com>
2018-09-22 19:46:46 +0300

    Tests for ActivityBuffer

--- domain/models/eventgroup.js
+++ domain/models/eventgroup.js
@@ -1,9 +1,7 @@
 'use strict'
 
 module.exports = (sequelize, DataTypes) => {
-  const EventGroup = sequelize.define('EventGroup', {
-
-  }, {})
+  const EventGroup = sequelize.define('EventGroup', {}, {})
 
   EventGroup.associate = (models) => {
     EventGroup.hasMany(models.Event, { foreignKey: "eventGroupId" })
