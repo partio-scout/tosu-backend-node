92d94dde4722b4854f1a37e6d1aded6d3a587043
virtalas <virtalas@users.noreply.github.com>
2018-09-24 16:43:59 +0300

    Tests for EventGroup, Plan and Scout

---
+++ tests/models/plan.test.js
@@ -0,0 +1,24 @@
+const supertest = require('supertest')
+const sequelize = require('sequelize')
+const models = require('../../domain/models')
+require('../testDatabase')
+
+test('Plan can be created', async () => {
+  const plan = await models.Plan.create({
+    title: "Quaint plan",
+    guid: "jgkdflhgjfkld",
+    content: "Do this and that"
+  })
+  expect(Number.isInteger(plan.id)).toBe(true)
+  expect(plan.title).toBe("Quaint plan")
+  expect(plan.guid).toBe("jgkdflhgjfkld")
+  expect(plan.content).toBe("Do this and that")
+})
+
+test('Plan can be assigned to Activity', async () => {
+  const activity = await models.Activity.create()
+  const plan = await models.Plan.create({ activityId: activity.id })
+
+  const fetchedPlan = await models.Plan.findById(plan.id, { include: [models.Activity] })
+  expect(fetchedPlan.Activity.id).toBe(activity.id)
+})
---
+++ tests/models/scout.test.js
@@ -0,0 +1,32 @@
+const supertest = require('supertest')
+const sequelize = require('sequelize')
+const models = require('../../domain/models')
+require('../testDatabase')
+
+test('Scout can be created', async () => {
+  const scout = await models.Scout.create({
+    googleId: "ghfjdklgh",
+    name: "Teppo"
+  })
+  expect(Number.isInteger(scout.id)).toBe(true)
+  expect(scout.googleId).toBe("ghfjdklgh")
+  expect(scout.name).toBe("Teppo")
+})
+
+test('Scout can be assigned Events', async () => {
+  const scout = await models.Scout.create({})
+  const event1 = await models.Event.create({ scoutId: scout.id })
+  const event2 = await models.Event.create({ scoutId: scout.id })
+  const event3 = await models.Event.create({ scoutId: scout.id })
+
+  const fetchedScout = await models.Scout.findById(scout.id, { include: [models.Event] })
+  expect(fetchedScout.Events.length).toBe(3)
+})
+
+test('Scout can be assigned an ActivityBuffer', async () => {
+  const scout = await models.Scout.create({})
+  const buffer = await models.ActivityBuffer.create({ scoutId: scout.id })
+
+  const fetchedScout = await models.Scout.findById(scout.id, { include: [models.ActivityBuffer] })
+  expect(fetchedScout.ActivityBuffer.id).toBe(buffer.id)
+})
