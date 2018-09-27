215760807fe42a3f860ce2f026223438eaacb856
hyytiala <otto.hyytiala@gmail.com>
2018-09-27 12:34:18 -0400

    travis pipeline

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -3,14 +3,12 @@ set -x
 eval "$(ssh-agent -s)"
 chmod 600 ~/.travis/id_rsa
 ssh-add ~/.travis/id_rsa
-ssh deploy@$IP -p $PORT <<EOF
+ssh ubuntu@$IP <<EOF
     if [[ $TRAVIS_BRANCH = 'master' ]]
     then
         cd ~/tosu-backend-node
         git pull
         npm install
-        pm2 restart index
 	echo "Frontside ollie ftw!"
     fi
 EOF
-
