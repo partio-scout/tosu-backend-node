883f0d1b620dd7f01ac456b2cb92daee7a194156
hyytiala <otto.hyytiala@gmail.com>
2018-09-30 18:30:47 +0300

    travis fix

--- .scripts/install.sh
+++ .scripts/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -x
 openssl enc -nosalt -aes-256-cbc -d -in tosu_node.pem.enc -out tosu_node.pem -base64 -K $encrypted_key -iv $encrypted_iv
-rm tosu_node.pem.enc
+ls -la
 sudo chmod 600 tosu_node.pem
 sudo mv tosu_node.pem ~/.travis/id_rsa
