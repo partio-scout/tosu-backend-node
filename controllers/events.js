32fcfb039523d63e305e0287b273700621afe596
Hansuzu <hannes.ihalainen@gmail.com>
2018-10-06 22:17:29 +0300

    Some changes on error handling

--- controllers/events.js
+++ controllers/events.js
@@ -2,12 +2,13 @@ const eventRouter = require('express').Router()
 const verifyService = require('../services/verifyService')
 const eventService = require('../services/eventService')
 
+// TODO: check if the scout is actually logged in ..?
 
 eventRouter.get('', async (req, res) => {
   const scout = req.session.scout
-  if (!scout){//TODO: CHECK here if the scout is actually logged in
+  if (!scout){
     res.status(403).send('you are not logged in!')
-  }else{ //TODO: does eventService.getAllEvents always succeed - handle exceptions
+  }else{
     const events = await eventService.getAllEvents(scout.id)
     res.status(200).send(events) 
   }
@@ -15,9 +16,9 @@ eventRouter.get('', async (req, res) => {
 
 eventRouter.post('', async (req, res) => {
   const scout = req.session.scout
-  if (!scout){ //TODO: Check here if scout is actually logged in
+  if (!scout){
     res.status(403).send('You are not logged in!')
-  }else{ //TODO: does eventService.createEvent always succeed?
+  }else{
     const event = await eventService.createEvent(scout.id, req.body)
     res.status(200).send(event)
   }
@@ -26,25 +27,29 @@ eventRouter.post('', async (req, res) => {
 eventRouter.put('/:eventId', async (req, res) => {
   const scout = req.session.scout
   const eventId=req.params.eventId
-  if (!verifyService.scoutOwnsEvent(scout, eventId)){ //TODO: Check here if scout is actually logged in
+  if (!verifyService.scoutOwnsEvent(scout, eventId)){
     res.status(403).send('You are not the owner of this event!')
-  }else{ //TODO: does eventService.updateEvent always succeed?
+  }else{
     const event=await eventService.updateEvent(eventId, req.body)
-    res.status(200).send(event)
+    if (event.error){ //Should never really happen since verifyService should prevent all errors
+      res.status(404).send(event.error)
+    }else{
+      res.status(200).send(event)
+    }
   }
 })
 
 eventRouter.delete('/:eventId', async (req, res) => {
   const scout = req.session.scout
   const eventId=req.params.eventId
-  if (!verifyService.scoutOwnsEvent(scout, eventId)){ //TODO: Check here if scout is actually logged in
+  if (!verifyService.scoutOwnsEvent(scout, eventId)){
     res.status(403).send('You are not the owner of this event!')
   }else{
     const succeeded=await eventService.deleteEvent(eventId)
     if (succeeded) {
       res.status(200).send('The event deleted.')
-    }else{
-      res.status(404).send('The event not deleted BUGI !!!111')
+    }else{ //Should not happen
+      res.status(404).send('The event was not deleted.')
     }
   }
 })
--- services/eventService.js
+++ services/eventService.js
@@ -1,8 +1,6 @@
 const models = require('../domain/models')
 
 
-// TODO: Add tests
-
 async function getAllEvents(scoutId) {
   const event = await models.Event.findAll({
     where:{
@@ -27,10 +25,9 @@ async function createEvent(scoutId, newEvent) {
 }
 
 async function updateEvent(eventId, newEvent) {
-  // This function returns the updated element only, TODO: proper error handling
   const event = await models.Event.findById(eventId)
   if (event === null){
-    return {}
+    return {error : 'Event was not found'}
   }else{
     const updated = await models.Event.update(
       {
@@ -48,8 +45,9 @@ async function updateEvent(eventId, newEvent) {
         }
       }
     )
-    if (updated[0] !== 1){
-      console.log('Some weird error happened when updating and event!!!!11', updated)
+    if (updated[0] !== 1){ //Should actually never happen
+      console.log('Some nasty and weird error occurred when updating an event', updated)
+      return {error : 'Something went wrong o_O'}
     }
     const updatedEvent = await models.Event.findById(event.id)
     return updatedEvent
