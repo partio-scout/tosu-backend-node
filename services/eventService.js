546e3d5da4dffdc95daa55f673e203bfb7b884ce
Hansuzu <hannes.ihalainen@gmail.com>
2018-10-07 19:29:11 +0300

    Added some whitespaces and removed an if-else to make code cleaner

--- services/eventService.js
+++ services/eventService.js
@@ -37,31 +37,30 @@ async function createEvent(scoutId, newEvent) {
 async function updateEvent(eventId, newEvent) {
   const event = await models.Event.findById(eventId)
   if (event === null){
-    return {error : 'Event was not found'}
-  }else{
-    const updated = await models.Event.update(
-      {
-        title: newEvent.title,
-        startDate: newEvent.startDate,
-        startTime: newEvent.startTime,
-        endDate: newEvent.endDate,
-        endTime: newEvent.endTime,
-        type: newEvent.type,
-        information: newEvent.information
-      }, 
-      {
-        where: {
-          id: {$eq: event.id}
-        }
-      }
-    )
-    if (updated[0] !== 1){ //Should actually never happen
-      console.log('Some nasty and weird error occurred when updating an event', updated)
-      return {error : 'Something went wrong o_O'}
-    }
-    const updatedEvent = await models.Event.findById(event.id)
-    return updatedEvent
+    return { error : 'Event was not found' }
   }
+  const updated = await models.Event.update(
+    {
+      title: newEvent.title,
+      startDate: newEvent.startDate,
+      startTime: newEvent.startTime,
+      endDate: newEvent.endDate,
+      endTime: newEvent.endTime,
+      type: newEvent.type,
+      information: newEvent.information
+    },
+    {
+      where: {
+        id: { $eq: event.id }
+      }
+    }
+  )
+  if (updated[0] !== 1) { //Should actually never happen
+    console.log('Some nasty and weird error occurred when updating an event', updated)
+    return { error : 'Something went wrong o_O' }
+  }
+  const updatedEvent = await models.Event.findById(event.id)
+  return updatedEvent
 }
 
 // Deletes an event
