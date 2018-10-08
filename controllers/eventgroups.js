d3f5cca346ee398633e692a71810fd166c9e0f4e
Otto Hyytiälä <otto.hyytiala@gmail.com>
2018-10-08 15:58:10 +0300

    Route for creating eventgroup

--- controllers/eventgroups.js
+++ controllers/eventgroups.js
@@ -1,9 +1,11 @@
 const eventgroupRouter = require('express').Router()
 const request = require('request')
 const axios = require('axios')
+const models = require('../domain/models')
 
 eventgroupRouter.post('/', async (req,res) => {
-  res.send('eventgoup')
+  const group = await models.EventGroup.create()
+  res.json(group.id)
 })
 
 eventgroupRouter.delete('/:id', async (req,res) => {
