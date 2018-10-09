331ceeb4451f1f8b09145de143272bc49ae40973
hyytiala <otto.hyytiala@gmail.com>
2018-10-09 13:57:53 +0300

    Refactored pof creation

--- tests/controllers/pof_api.test.js
+++ tests/controllers/pof_api.test.js
@@ -2,10 +2,23 @@ const supertest = require('supertest')
 const { app, server } = require('../../index')
 const api = supertest(app)
 
-test('Backend works', async () => {
+test('Pof backend works', async () => {
   await api
     .get('/filledpof')
     .expect(200)
+})
+
+test('Backend returns JSON', async () => {
+  await api
+    .get('/filledpof/tarppo')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('Backend returns tarpojat data', async () => {
+  const response = await api
+    .get('/filledpof/tarppo')
+  expect(response.body.title).toBe('Tarpojat (12-15v.)')
 })
 
 afterAll(() => {
