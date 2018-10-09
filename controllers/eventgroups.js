42d8814b8bf99a4732c43f9dba6a4fb12c791813
Otto Hyytiälä <otto.hyytiala@gmail.com>
2018-10-09 15:26:26 +0300

    delete all events within eventgroup

--- controllers/eventgroups.js
+++ controllers/eventgroups.js
@@ -1,22 +1,32 @@
 const eventgroupRouter = require('express').Router()
 const models = require('../domain/models')
 
-eventgroupRouter.post('/', async (req,res) => {
+eventgroupRouter.post('/', async (req, res) => {
   const group = await models.EventGroup.create()
   res.json(group.id)
 })
 
-eventgroupRouter.delete('/:id', async (req,res) => {
+eventgroupRouter.delete('/:id', async (req, res) => {
   const groupId = req.params.id
   models.EventGroup.destroy({
     where: {
-      id: { $eq: groupId }
+      id: {$eq: groupId}
     }
   }).then(rowsDeleted => {
     //TODO: Delete events with this grooupId
     if (rowsDeleted === 1) {
-      console.log('Deleted activity with ID', req.params.id)
-      res.status(204).send('Deleted')
+      models.Event.destroy({
+        where: {
+          eventGroupId: {$eq: groupId}
+        }
+      }).then(rowsDeleted => {
+        if (rowsDeleted > 0) {
+          res.status(204)
+        } else {
+          console.log('Did not delete activity with ID')
+          res.status(404).send('Not deleted')
+        }
+      })
     } else {
       console.log('No eventgroup with ID', req.params.id)
       res.status(404).send('Not deleted')
