989c222b7ab1b3bbf306cc3370a298fb3c5a9bfd
Otto Hyytiälä <otto.hyytiala@gmail.com>
2018-10-11 13:39:31 +0300

    Update eventgroups.js

--- controllers/eventgroups.js
+++ controllers/eventgroups.js
@@ -13,22 +13,10 @@ eventgroupRouter.delete('/:id', async (req, res) => {
       id: {$eq: groupId}
     }
   }).then(rowsDeleted => {
-    //TODO: Delete events with this grooupId
     if (rowsDeleted === 1) {
-      models.Event.destroy({
-        where: {
-          eventGroupId: {$eq: groupId}
-        }
-      }).then(rowsDeleted => {
-        if (rowsDeleted > 0) {
-          res.status(204)
-        } else {
-          console.log('Did not delete activity with ID')
-          res.status(404).send('Not deleted')
-        }
-      })
+      res.status(204).send()
     } else {
-      console.log('No eventgroup with ID', req.params.id)
+      console.log('Did not delete activity with ID')
       res.status(404).send('Not deleted')
     }
   })
