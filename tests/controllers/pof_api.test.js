4e447d1db0761a24349c3b8dcd1da6ba3643db86
hyytiala <otto.hyytiala@gmail.com>
2018-11-01 12:03:35 +0200

    jest timeOut for travis

--- tests/controllers/pof_api.test.js
+++ tests/controllers/pof_api.test.js
@@ -1,6 +1,12 @@
 const supertest = require('supertest')
 const { app, server } = require('../../index')
 const api = supertest(app)
+var response
+
+beforeAll( async () => {
+  jest.setTimeout(30000)
+  response = await api.get('/filledpof/test')
+})
 
 test('Pof backend works', async () => {
   await api
@@ -12,37 +18,25 @@ test('Pof backend works', async () => {
 })
 
 test('Get tarpojaohjelma', async () => {
-  const response = await api
-    .get('/filledpof/test')
-    .expect(200)
   expect(response.body.title).toBe('Tarpojat (12-15v.)')
 })
 
 test('Get from cache', async () => {
-  const response = await api
+  const cache = await api
     .get('/filledpof/tarppo')
     .expect(200)
-  expect(response.body.title).toBe('Tarpojat (12-15v.)')
+  expect(cache.body.title).toBe('Tarpojat (12-15v.)')
 })
 
 test('Get details', async () => {
-  const response = await api
-    .get('/filledpof/tarppo')
-    .expect(200)
   expect(response.body.taskgroups[0].tasks[0].type).toBe('task')
 })
 
 test('Get suggestions', async () => {
-  const response = await api
-    .get('/filledpof/tarppo')
-    .expect(200)
   expect(response.body.taskgroups[0].tasks[0].suggestions_details.length > 1).toBe(true)
 })
 
 test('Get paussit', async () => {
-  const response = await api
-    .get('/filledpof/tarppo')
-    .expect(200)
   expect(response.body.taskgroups[1].taskgroups[0].taskgroups[0].tasks[0].task_term.name).toBe('paussi')
 })
 
