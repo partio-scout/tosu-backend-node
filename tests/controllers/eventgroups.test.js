309f153d8d775e8e36a09677d891b7a85991f228
pelsaara <saara.peltomaki@gmail.com>
2018-11-08 13:11:47 +0200

    Tests created for EventGroupRouter

--- tests/controllers/eventgroups.test.js
+++ tests/controllers/eventgroups.test.js
@@ -4,3 +4,27 @@ const api = supertest(app)
 const models = require('../../domain/models')
 const testUtils = require('../testUtils')
 require('../handleTestDatabase')
+
+
+test('Create an event group', async () => {
+  const result = await api.post('/eventgroups')
+    .send()
+    .expect('Content-Type', /json/)
+    .expect(200)
+  const eventGroupId = result.body.id
+
+  const dbEventGroup = await models.EventGroup.findById(eventGroupId)
+
+  expect(dbEventGroup.id).toBe(eventGroupId)
+
+})
+
+test('Delete event group', async () => {
+  const eventGroup = await models.EventGroup.create()
+
+  const result = await api.delete('/eventgroups/'+eventGroup.id)
+    .expect(204)
+
+  const dbEventGroup = await models.EventGroup.findById(eventGroup.id)
+  expect(dbEventGroup).toBe(null)
+})
