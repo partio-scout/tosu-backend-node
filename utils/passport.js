d7a5b7e426bacf2f0a85e381f919463cfda25474
virtalas <virtalas@users.noreply.github.com>
2018-12-04 15:44:41 +0200

    Load metadata for logout route

--- utils/passport.js
+++ utils/passport.js
@@ -4,7 +4,7 @@ const SamlStrategy = require('passport-saml').Strategy
 module.exports = function (passport, config) {
   metadata.fetch(config.passport.saml.metadata, {credentials: 'include'}) // credentials not necessary(?)
     .then(function (reader) {
-      const strategyConfig = metadata.toPassportConfig(reader);
+      const strategyConfig = metadata.toPassportConfig(reader)
       strategyConfig.realm = config.passport.saml.issuer
       strategyConfig.issuer = config.passport.saml.issuer
       strategyConfig.protocol = 'samlp'
@@ -12,26 +12,26 @@ module.exports = function (passport, config) {
       passport.use('saml', new SamlStrategy(strategyConfig, function (profile, done) {
         console.log("profile:",profile)
         // profile = metadata.claimsToCamelCase(profile, reader.claimSchema)
-        return done(null, profile);
+        return done(null, profile)
       }));
 
       // Save user to session
       passport.serializeUser(function(user, done) {
         console.log("serializeUser:", user)
-        done(null, user);
+        done(null, user)
       });
 
       // Attach user to req.user
       passport.deserializeUser(function(user, done) {
         console.log("deserializeUser:", user)
-        done(null, user);
+        done(null, user)
       });
       console.log("------------SAML metadata:", strategyConfig)
       console.log("------------")
 
     })
     .catch((err) => {
-      console.error('Error loading SAML metadata', err);
-      process.exit(1);
-    });
-};
+      console.error('Error loading SAML metadata', err)
+      process.exit(1)
+    })
+}
