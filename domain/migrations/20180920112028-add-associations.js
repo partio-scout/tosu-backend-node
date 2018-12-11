5e37c920a9460c88b5b29bb9f07b0e6e5050740c
Hansuzu <hannes.ihalainen@gmail.com>
2018-12-11 13:23:51 +0200

    important comment

--- domain/migrations/20180920112028-add-associations.js
+++ domain/migrations/20180920112028-add-associations.js
@@ -85,6 +85,7 @@ module.exports = {
 
   down: (queryInterface, Sequelize) => {
     // remove added columns in reversed order
+    // LOL, it wouldn't actually even matter :Dd
     return queryInterface.removeColumn('Plans', 'activityId').then(() => {
       return queryInterface.removeColumn('Events', 'scoutId')
     }).then(() => {
