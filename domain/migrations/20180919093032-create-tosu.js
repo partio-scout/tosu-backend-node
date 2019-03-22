aacd437b5f5301c6de16810676a3422a48c7b92a
Pavel Garmuyev <pavel.garmuyev@gmail.com>
2019-03-22 22:50:55 +0200

    Update migrations

--- domain/migrations/20180919093032-create-tosu.js
+++ domain/migrations/20180919093032-create-tosu.js
@@ -3,6 +3,12 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.createTable('Tosus', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
       name: {
         type: Sequelize.STRING,
         allowNull: false,
@@ -10,6 +16,14 @@ module.exports = {
       selected: {
         type: Sequelize.BOOLEAN,
         defaultValue: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
       },
     })
   },
